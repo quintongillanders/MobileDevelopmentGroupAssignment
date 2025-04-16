482f61e300dd9798b9680e2b89164b069a47a155
quintongillanders <quingillanders@gmail.com>
2025-04-16 13:11:27 +1200

    Update CustomerActivity.Java

--- app/src/main/java/com/example/stockhive/CustomerActivity.java
+++ app/src/main/java/com/example/stockhive/CustomerActivity.java
@@ -1,14 +1,51 @@
-package com.example.stockhive;
+package com.example.assignment2;
 
+import android.content.Context;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class CustomerActivity extends AppCompatActivity {
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.stockhive.ADApt1;
+import com.example.stockhive.R;
+
+public class Customer extends AppCompatActivity {
+
+    private Context context;
+
+    RecyclerView recyclerView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_customer);
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_customer);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.cust), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
+
+        String data[] = {"Bike1", "Bike2","Bike3","Bike4","Bike1", "Bike2","Bike3",
+                "Bike4","Bike1", "Bike2","Bike3","Bike4","Bike1", "Bike2","Bike3","Bike4",
+                "Bike1", "Bike2","Bike3","Bike4"};
+        int[] imageData = {R.drawable.bike1, R.drawable.bike2, R.drawable.bike3, R.drawable.bike4,
+                R.drawable.bike1, R.drawable.bike2, R.drawable.bike3, R.drawable.bike4,
+                R.drawable.bike1, R.drawable.bike2, R.drawable.bike3, R.drawable.bike4,
+                R.drawable.bike1, R.drawable.bike2, R.drawable.bike3, R.drawable.bike4,
+                R.drawable.bike1, R.drawable.bike2, R.drawable.bike3, R.drawable.bike4,};
+
+        recyclerView = findViewById(R.id.rec_view);
+        ADApt1 adapter = new ADApt1(imageData, data);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
 
     }
 }
