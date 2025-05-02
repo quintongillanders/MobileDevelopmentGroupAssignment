75a48022868357dc7891e052812ad194d44a6058
quintongillanders <quingillanders@gmail.com>
2025-05-02 17:14:20 +1200

    Product Admin Add Complete, Delete and update in progress

--- app/src/main/java/com/example/stockhive/ProductAdminActivity.java
+++ app/src/main/java/com/example/stockhive/ProductAdminActivity.java
@@ -1,6 +1,10 @@
 package com.example.stockhive;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ProductAdminActivity extends AppCompatActivity {
+
+    Button btnaddProduct, btnUpdateProduct, btnDeleteProduct, btnLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,44 @@ public class ProductAdminActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btnaddProduct = findViewById(R.id.btnAdd);
+        btnUpdateProduct = findViewById(R.id.btnUpdate);
+        btnDeleteProduct = findViewById(R.id.btnDelete);
+        btnLogout = findViewById(R.id.btnLogout);
+
+        btnaddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ProductAdminActivity.this, ProductAdminAddActivity.class);
+                startActivity(i);
+                Toast.makeText(ProductAdminActivity.this, "Add Product clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        btnUpdateProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ProductAdminActivity.this, ProductAdminUpdateActivity.class);
+                startActivity(i);
+                Toast.makeText(ProductAdminActivity.this, "Update Product clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        btnDeleteProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ProductAdminActivity.this, ProductAdminDeleteActivity.class);
+                startActivity(i);
+                Toast.makeText(ProductAdminActivity.this, "Delete Product clicked", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/stockhive/ProductAdminUpdateActivity.java
@@ -0,0 +1,33 @@
+package com.example.stockhive;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ProductAdminUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_product_admin_update);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
