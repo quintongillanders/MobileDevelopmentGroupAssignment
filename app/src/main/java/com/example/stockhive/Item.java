a96e18fe03c65d2d874dedd28ca5e71817302b92
PeterNelaturi <peter1985.raj@gmail.com>
2025-05-31 18:19:49 +1200

    Fixed ADD Product in Supplier

---
+++ app/src/main/java/com/example/stockhive/Item.java
@@ -0,0 +1,47 @@
+package com.example.stockhive;
+
+public class Item {
+    private String itemname;
+
+    private int itemquantity;
+
+    private String id;
+
+    public Item() {
+    }
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Item(String itemname, int itemquantity) {
+        this.itemname = itemname;
+        this.itemquantity = itemquantity;
+    }
+
+    public String getItemname() {
+        return itemname;
+    }
+
+    public void setItemname(String itemname) {
+        this.itemname = itemname;
+    }
+
+    public int getItemquantity() {
+        return itemquantity;
+    }
+
+    public void setItemquantity(int itemquantity) {
+        this.itemquantity = itemquantity;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/stockhive/SupplierAdapter.java
@@ -0,0 +1,59 @@
+package com.example.stockhive;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SupplierAdapter extends RecyclerView.Adapter<ProductViewHolder> {
+
+    private ArrayList<Item> itemList;
+
+    public SupplierAdapter(ArrayList<Item> itemList) {
+        this.itemList = itemList;
+    }
+
+    public void setData(ArrayList<Item> itemList) {
+        this.itemList = itemList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ProductViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.supplier_items,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductViewHolder holder, int position) {
+        Item item = itemList.get(position);
+        holder.itemnameTV.setText(item.getItemname());
+        holder.itemquantityTV.setText("Quantity:" +Integer.toString(item.getItemquantity()));
+
+
+        holder.btnDelete.setOnClickListener(v -> {
+
+            FirebaseDatabase.getInstance().getReference()
+                    .child("items")
+                    .child(item.getId())
+                    .removeValue();
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+
+
+
+}
+
